fccceb75feaa237754086a94a260b1b4667f721f
Irving <zhouyongtao@outlook.com>
2014-05-11 01:10:30 +0800

    增加代码

--- src/main/java/com/homeinns/web/controller/LoginController.java
+++ src/main/java/com/homeinns/web/controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.homeinns.web.controller;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class LoginController {
-	
+	public String name() {
+		return "Irving";
+	}
 }
