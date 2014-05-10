11964e2d7e8cbbedd10ed2a347945194d764b22d
Irving <zhouyongtao@outlook.com>
2014-05-11 00:57:20 +0800

    修改代码

--- src/test/java/com/homeinns/web/test/TestUserService.java
+++ src/test/java/com/homeinns/web/test/TestUserService.java
@@ -1,7 +1,7 @@
 package com.homeinns.web.test;
-import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.Test;
 
 import com.homeinns.web.service.impl.UserServiceImpl;
 
@@ -14,7 +14,7 @@ public class TestUserService {
 		ApplicationContext contextClassPath = new ClassPathXmlApplicationContext("applicationContext.xml");
 		// ApplicationContext contextFileSystem = new  FileSystemXmlApplicationContext("WebRoot/WEB-INF/applicationContext.xml");
 		UserServiceImpl userImpl = (UserServiceImpl) contextClassPath.getBean(UserServiceImpl.class);
-		String result = userImpl.sayName("Irving");
+		String result = userImpl.sayName("Irving O(∩_∩)O~~");
 		System.out.println(result);
 	}
 }
